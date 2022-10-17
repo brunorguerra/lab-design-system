e94486f71218eaa38e2793300a80f3a8cc8dd7c0
Bruno <bruno.dev8801@gmail.com>
2022-10-17 20:00:18 -0300

    introduzed testing library and created pages dir

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -7,6 +7,7 @@ module.exports = {
         "@storybook/addon-links",
         "@storybook/addon-essentials",
         "@storybook/addon-interactions",
+        "@storybook/addon-a11y",
     ],
     framework: "@storybook/react",
     core: {
@@ -14,7 +15,9 @@ module.exports = {
     },
     features: {
         storyStoreV7: true,
+        interactionsDebugger: true,
     },
+    staticDirs: ["../public"],
     viteFinal: (config, { configType }) => {
         if (configType === "PRODUCTION") {
             config.base = "/lab-design-system/";
